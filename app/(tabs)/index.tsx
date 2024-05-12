4b1a3182a1a2167e5a94857f1e672be3e4612111
BladedShip <ultimateallyn@gmail.com>
2024-05-13 02:05:22 +0530

    Added carousel + setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,118 @@
-import { StyleSheet } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text, useWindowDimensions, Image, Pressable } from "react-native";
+import { useSharedValue } from "react-native-reanimated";
+import Carousel, { ICarouselInstance } from "react-native-reanimated-carousel";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+const productImages = [
+  {
+    id: 1,
+    img: require("../../assets/images/product-header-1.png"),
+  },
+  {
+    id: 2,
+    img: require("../../assets/images/product-header-2.png"),
+  },
+  {
+    id: 3,
+    img: require("../../assets/images/product-header-3.png"),
+  },
+];
 
 export default function TabOneScreen() {
+  const windowWidth = useWindowDimensions().width;
+  const scrollOffsetValue = useSharedValue<number>(0);
+  const [isVertical, setIsVertical] = React.useState(false);
+  const ref = React.useRef<ICarouselInstance>(null);
+  const [currentImageIndex, setCurrentImageIndex] = React.useState(0);
+  const onScroll = React.useCallback((index: number) => {
+    setCurrentImageIndex(index);
+  }, []);
+
+  const PAGE_WIDTH = windowWidth;
+  const baseOptions = isVertical
+    ? ({
+        vertical: true,
+        width: windowWidth,
+        height: PAGE_WIDTH / 1,
+      } as const)
+    : ({
+        vertical: false,
+        width: windowWidth,
+        height: PAGE_WIDTH / 1,
+      } as const);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <SafeAreaView edges={["bottom", "top"]} style={{ flex: 1 }}>
+        <View>
+          <Carousel
+            {...baseOptions}
+            onSnapToItem={onScroll}
+            ref={ref}
+            defaultScrollOffsetValue={scrollOffsetValue}
+            style={{ width: "100%" }}
+            data={productImages}
+            renderItem={({ index }) => (
+              <View
+                key={index}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Image
+                  source={productImages[index].img}
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    resizeMode: "contain",
+                  }}
+                />
+              </View>
+            )}
+          />
+          <View
+            style={{
+              position: "absolute",
+              bottom: 4,
+              right: 0,
+              left: 0,
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: 8,
+            }}
+          >
+            {productImages.map((_, index) => (
+              <Pressable
+                onPress={() => {
+                  ref.current?.scrollTo({
+                    index,
+                    animated: true,
+                  });
+                }}
+                key={index}
+              >
+                <Image
+                  source={productImages[index].img}
+                  style={{
+                    width: 64,
+                    height: 64,
+                    borderRadius: 10,
+                    margin: 5,
+                    borderColor: currentImageIndex === index ? "#156651" : "transparent",
+                    backgroundColor: "white",
+                    borderWidth: 2,
+                  }}
+                />
+              </Pressable>
+            ))}
+          </View>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
@@ -16,16 +120,6 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    backgroundColor: "#fff",
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,26 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-
-import { useColorScheme } from '@/components/useColorScheme';
-
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import {
+  Manrope_200ExtraLight,
+  Manrope_300Light,
+  Manrope_400Regular,
+  Manrope_500Medium,
+  Manrope_600SemiBold,
+  Manrope_700Bold,
+  Manrope_800ExtraBold,
+} from "@expo-google-fonts/manrope";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -22,7 +28,13 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    Manrope_200ExtraLight,
+    Manrope_300Light,
+    Manrope_400Regular,
+    Manrope_500Medium,
+    Manrope_600SemiBold,
+    Manrope_700Bold,
+    Manrope_800ExtraBold,
     ...FontAwesome.font,
   });
 
@@ -45,14 +57,16 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <GestureHandlerRootView
+      style={{
+        flex: 1,
+      }}
+    >
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
-    </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
